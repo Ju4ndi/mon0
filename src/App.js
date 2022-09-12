06ced182094bfddd495dd8ea221ceb4b9f7a1aa3
Ju4ndi <juan@qualmark.io>
2022-09-12 14:29:12 -0500

    theme imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
-import Dashboard from './Features/Dashboard';
+import GlobalStyles from './Styles'
+import Dashboard from './Features/Dashboard'
 
 function App() {
   return (
+    <>
+    <GlobalStyles />
     <Dashboard/>
-  );
+    </>
+  )
 }
 
 export default App;

2cc00d5fbce1f1107da750e37aea105d25de2b9b
Ju4ndi <juan@qualmark.io>
2022-09-13 10:42:55 -0500

    files unnecesary deleted

--- src/Components/Welcome/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ContainerStyled from "./styles"
-
-function Hello() { 
-  return <ContainerStyled><div> Hi World From Hello </div></ContainerStyled>
-}
-
-export default Hello
